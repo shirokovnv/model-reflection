b223931e05faa560e84d7f1192ad0c45bc10278b
shirokovnv <shirokovnv@gmail.com>
2022-08-18 20:00:00 +0500

    Remove config

--- src/ModelReflectionServiceProvider.php
+++ src/ModelReflectionServiceProvider.php
@@ -10,27 +10,12 @@ use Illuminate\Support\ServiceProvider;
 class ModelReflectionServiceProvider extends ServiceProvider
 {
     /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot(): void
-    {
-        // Publishing is only necessary when using the CLI.
-        if ($this->app->runningInConsole()) {
-            $this->bootForConsole();
-        }
-    }
-
-    /**
      * Register any package services.
      *
      * @return void
      */
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/model-reflection.php', 'model-reflection');
-
         // Register the service the package provides.
         $this->app->singleton('model-reflection', function ($app) {
             return new ModelReflection(DB::connection());
@@ -45,18 +30,5 @@ class ModelReflectionServiceProvider extends ServiceProvider
     public function provides(): array
     {
         return ['model-reflection'];
-    }
-
-    /**
-     * Console-specific booting.
-     *
-     * @return void
-     */
-    protected function bootForConsole(): void
-    {
-        // Publishing the configuration file.
-        $this->publishes([
-            __DIR__ . '/../config/model-reflection.php' => config_path('model-reflection.php'),
-        ], 'model-reflection.config');
     }
 }
